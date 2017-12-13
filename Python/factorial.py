c216737ada566384c43a3c9d755b9bcb1ace8063
ivan-climb1 <vindieselvsall@gmail.com>
2017-12-13 15:45:32 +0300

    Complete factorial

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,19 +1,33 @@
 """
-    This program
+    This program ask the user enter a positive integer,
+    then calculates the factorial of a number.
 """
-
-n = int(input("Pleaze, give positive integer\n"))
-
 def main():
-    if n < 0:
-        print("Not negativ!!")
-        exit(1)
-    print(fac(n))
+
+    # request a string from the user
+    number = input("Hi! Pleaze, give me a positive integer\n")
+
+    # if the string consist not only of digits,
+    # then again request a string from the user
+    while not(number.isdigit()):
+        number = input("Give me only a positive integer\n")
+
+    # get an integer number from a string
+    number = int(number)
+
+    # call the function fac(number), and print factorial
+    print("The factorial of this number", fac(number))
+
 
 def fac(number):
+
     if number == 0:
         return 1
+
+    # recursively call the function fac(number - 1)
     return(number * fac(number - 1))
+
 
 if __name__ == "__main__":
     main()
+
